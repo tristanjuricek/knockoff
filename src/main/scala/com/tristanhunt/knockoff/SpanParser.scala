c3f82f5e68f5ebc6632387b6765953ea874e1616
Tristan Juricek <juricek@emarsys.com>
2009-03-18 07:32:17 +0100

    Minor changes, split up of function.

---
+++ src/main/scala/com/tristanhunt/knockoff/SpanParser.scala
@@ -0,0 +1,15 @@
+package com.tristanhunt.knockoff
+
+import util.parsing.combinator._
+
+/**
+ * Note that this will operate on already identified text segments only: that is, no inline HTML,
+ * because any characters like `<` or `>` will be automatically replaced.
+ *
+ * @author Tristan Juricek <mr.tristan@gmail.com>
+ */
+object SpanParser
+extends JavaTokenParsers {
+
+
+}
