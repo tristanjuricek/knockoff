0643e817e8e6649e69a64d1314c546fb83072f10
Tristan Juricek <juricek@emarsys.com>
2009-03-30 23:21:29 +0200

    Added the tabs test, with a few minor modifications. The tabbing thing might be a little funky.

--- src/test/scala/com/tristanhunt/knockoff/MarkdownTestSuite.scala
+++ src/test/scala/com/tristanhunt/knockoff/MarkdownTestSuite.scala
@@ -217,7 +217,11 @@ class MarkdownTestSuite {
             "</code></pre>" +
         "</blockquote>"
     )
-        
+    
+    def tabs = _compare(
+        MarkdownExamples.tabs,
+        ConversionExamples.tabs
+    )        
 
     /**
      * Doing a couple of tricks so that each of the tests are just a bit easier to read.
@@ -311,6 +315,29 @@ Plus: `\+`
 
 Minus: `\-`
 """
+
+    val tabs = """+	this is a list item
+	indented with tabs
+
++   this is a list item
+    indented with spaces
+
+Code:
+
+	this code block is indented by one tab
+
+And:
+
+		this code block is indented by two tabs
+
+And:
+
+	+   this is an example list item
+		indented with tabs
+    
+	+   this is an example list item
+	    indented with spaces
+"""
 }
 
 /**
@@ -372,4 +399,19 @@ Minus: \-
 </p><p>Minus: <code>\-</code>
 </p>"""
 
+    val tabs = """<ul><li>this is a list item
+    indented with tabs
+</li><li>this is a list item
+    indented with spaces
+</li></ul><p>Code:
+</p><pre><code>this code block is indented by one tab
+</code></pre><p>And:
+</p><pre><code>    this code block is indented by two tabs
+</code></pre><p>And:
+</p><pre><code>+   this is an example list item
+    indented with tabs
+
++   this is an example list item
+    indented with spaces
+</code></pre>"""
 }
