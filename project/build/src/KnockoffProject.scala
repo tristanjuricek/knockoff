51108fd9f8384750ed9ba8b0cd2a9b1d7cd9f23d
Tristan Juricek <juricek@emarsys.com>
2009-08-16 10:08:26 +0200

    Changed configuration to my deployment server.

--- project/build/src/KnockoffProject.scala
+++ project/build/src/KnockoffProject.scala
@@ -1,8 +1,13 @@
 import sbt._
 
 class KnockoffProject( info : ProjectInfo ) extends DefaultProject( info ) {
-    
-    val emarsysNexus = "emarsys Nexus" at "http://nexus.emarsys.com/content/groups/public"
+
+    Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+    val nexus = "tristanhunt" at "http://tristanhunt.com:8081/content/groups/public/"
 
     val ScalaTest = "org.scala-tools.testing" % "scalatest" % "0.9.5"
+    
+    override def managedStyle = ManagedStyle.Maven
+    val publishTo = "tristanhunt releases" at "http://tristanhunt.com:8081/content/repositories/releases/"
 }
