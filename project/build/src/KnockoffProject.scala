80013371abdb6427b2ec6d9a42a05dfe75832815
Tristan Juricek <mr.tristan@gmail.com>
2010-07-14 11:46:32 -0700

    0.7.3-14 : Fixes mostly related to extensions and documentation revision.

--- project/build/src/KnockoffProject.scala
+++ project/build/src/KnockoffProject.scala
@@ -11,10 +11,13 @@ class KnockoffProject( info : ProjectInfo ) extends DefaultProject( info )
   override def mainClass = Some("com.tristanhunt.knockoff.DefaultDiscounter")
   
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-  val nexus = "tristanhunt" at "http://tristanhunt.com:8081/content/groups/public/"
-
+  
+  val tristanhunt          = "tristanhunt" at "http://tristanhunt.com:8081/content/groups/public/"
+  val tristanhuntSnapshots = "tristanhunt Snapshots" at "http://tristanhunt.com:8081/content/groups/public-snapshots/"
+  
   // TODO -> For 2.8.0.* series, figure out how to change test dependency.
-  // val scala_test = "org.scalatest" % "scalatest" % "1.0.1" % "test->default"
+  // val scala_test = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT" % "test->default"
+  val scala_test = "org.scalatest" % "scalatest" % "1.0.1" % "test->default"
   val jtidy = "jtidy" % "jtidy" % "r938" % "test->default"
   val snuggletex = "uk.ac.ed.ph.snuggletex" % "snuggletex-core" % "1.2.2"
     
