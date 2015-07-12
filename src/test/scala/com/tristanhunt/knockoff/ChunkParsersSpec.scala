89fec4cca4364b36212d1e15e39ead4779c1b105
linerlock <linerlock@outlook.com>
2015-07-12 10:37:05 +0900

    Change specs to match the issue #44

--- src/test/scala/com/tristanhunt/knockoff/ChunkParsersSpec.scala
+++ src/test/scala/com/tristanhunt/knockoff/ChunkParsersSpec.scala
@@ -12,31 +12,31 @@ class ChunkParsersSpec extends ChunkParser with FunSpecLike with ShouldMatchers 
   describe("ChunkParser") {
     it("should handle simple bullet items") {
       val src = "* item 1\n* item 2\n"
-      parse( chunk, src ).get should equal ( BulletLineChunk("item 1\n") )
+      parse( chunk, src ).get should equal ( BulletLineChunk("item 1") )
     }
-    
+
     it("should group a second line that's not a bullet") {
       val src = "*   item 1\n    more\n"
       parse( chunk, src ).get should equal (
-        BulletLineChunk("item 1\nmore\n")
+        BulletLineChunk("item 1\nmore")
       )
     }
-    
+
     it("should ignore whitespace around headers") {
       val src = "# Header 1 #"
       parse( chunk, src ).get should equal { HeaderChunk(1, "Header 1") }
     }
-    
+
     it("should be ok with empty code blocks") {
       val src = "    "
       parse( chunk, src ).get should equal { IndentedChunk("") }
     }
-    
+
     it("should not explode on a code block with a trailing line") {
       val line = "    line\n    "
-      parse( chunk, line ).get should equal { IndentedChunk("line\n") }
+      parse( chunk, line ).get should equal { IndentedChunk("line") }
     }
-    
+
     it("should handle nothin' but code") {
       val src = "    This is just a code block.\n" +
                 "    \n" +
@@ -45,15 +45,15 @@ class ChunkParsersSpec extends ChunkParser with FunSpecLike with ShouldMatchers 
       parse( chunk, src ).get should equal { IndentedChunk(
         "This is just a code block.\n" +
         "\n" +
-        "And it has a trailing whitespace line... that's also indented.\n"
+        "And it has a trailing whitespace line... that's also indented."
       ) }
     }
-    
+
     it("should deal with a CRLF") {
       val src = "This is a line \r\nthat is broken in\r\n a couple places.\r\n"
-      parse( chunk, src ).get should equal { TextChunk(src) }
+      parse( chunk, src ).get should equal { TextChunk(src.stripSuffix("\r\n")) }
     }
-    
+
     it("should deal with just a CRLF") {
       val src = "\u000d\u000a"
       parse( chunk, src ).get should equal { EmptySpace(src) }
